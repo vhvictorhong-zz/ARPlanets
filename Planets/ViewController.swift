df2d05006b1f2015ed41706398ab5fc38a28a1cc
victorhong1988 <victorhong1988@gmail.com>
2017-12-18 11:46:11 -0500

    Fix Earth lighting and added Venus

--- Planets/ViewController.swift
+++ Planets/ViewController.swift
@@ -19,27 +19,43 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
         self.sceneView.session.run(configuration)
+        self.sceneView.autoenablesDefaultLighting = true
         
     }
 
     override func viewDidAppear(_ animated: Bool) {
         
-        let earthNode = SCNNode()
-        earthNode.geometry = SCNSphere(radius: 0.2)
-        earthNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "2k_earth_daymap")
-        earthNode.position = SCNVector3(0, 0, -1)
-        self.sceneView.scene.rootNode.addChildNode(earthNode)
-        
+        let sunNode = SCNNode(geometry: SCNSphere(radius: 0.35))
+        sunNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Sun Diffuse")
+        sunNode.position = SCNVector3(0, 0, -1)
+        self.sceneView.scene.rootNode.addChildNode(sunNode)
+        let earthNode = planet(geometry: SCNSphere(radius: 0.2), diffuse: #imageLiteral(resourceName: "Earth day"), specular: #imageLiteral(resourceName: "Earth Specular"), emission: #imageLiteral(resourceName: "Earth Emission"), normal: #imageLiteral(resourceName: "Earth Normal"), position: SCNVector3(1.2, 0, 0))
+        sunNode.addChildNode(earthNode)
+        let venus = planet(geometry: SCNSphere(radius: 0.1), diffuse: #imageLiteral(resourceName: "Venus Surface"), specular: nil, emission: #imageLiteral(resourceName: "Venus Atmosphere"), normal: nil, position: SCNVector3(0.7, 0, 0))
+        sunNode.addChildNode(venus)
         let action = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 8)
         let forever = SCNAction.repeatForever(action)
         earthNode.runAction(forever)
         
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func planet(geometry: SCNGeometry, diffuse: UIImage, specular: UIImage?, emission: UIImage?, normal: UIImage?, position: SCNVector3) -> SCNNode {
+        
+        let planet = SCNNode(geometry: geometry)
+        planet.geometry?.firstMaterial?.diffuse.contents = diffuse
+        planet.geometry?.firstMaterial?.specular.contents = specular
+        planet.geometry?.firstMaterial?.emission.contents = emission
+        planet.geometry?.firstMaterial?.normal.contents = normal
+        planet.position = position
+
+        return planet
+        
+    }
 
 }
 
