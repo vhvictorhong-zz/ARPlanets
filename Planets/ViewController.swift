4ef6f6259ece4d62aa035b4114121defc0b610ff
victorhong1988 <victorhong1988@gmail.com>
2017-12-18 10:53:38 -0500

    Created planet earth

--- Planets/ViewController.swift
+++ Planets/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+        
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        
+        let earthNode = SCNNode()
+        earthNode.geometry = SCNSphere(radius: 0.2)
+        earthNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "2k_earth_daymap")
+        earthNode.position = SCNVector3(0, 0, -1)
+        self.sceneView.scene.rootNode.addChildNode(earthNode)
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
